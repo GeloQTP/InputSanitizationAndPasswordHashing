9010e46a0175d052f09daabb404324b763dc3e81
GeloQTP <geloadrian24@gmail.com>
2025-12-23 20:48:13 +0800

    added Users Table in the admin dashboard

--- pages/adminDashboard.php
+++ pages/adminDashboard.php
@@ -2,6 +2,15 @@
 require("../includes/db_connect.php");
 ?>
 
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$statement = $conn->prepare("SELECT * FROM users");
+$statement->execute();
+
+$result = $statement->get_result();
+$rows = $result->fetch_assoc();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +18,7 @@ require("../includes/db_connect.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/dashBoard.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
 </head>
 
 <body>
@@ -25,6 +34,15 @@ require("../includes/db_connect.php");
     <main>
 
         <h1>Welcome to the Admin Dashboard</h1>
+        <br>
+        <table border="1" cellspacing="5" cellpadding="5" width="50%" height="200" align="center" bgcolor="#f2f2f2">
+            <tr>
+                <th>Username</th>
+                <th>Registration Date</th>
+                <th>User Type</th>
+            </tr>
+
+        </table>
 
     </main>
 
